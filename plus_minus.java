f6b5b042b90eadcf7903b5b9b461df6fc55a3d11
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-24 14:23:53 +0530

    repo updated by adding new solution

---
+++ plus_minus.java
@@ -0,0 +1,32 @@
+/*
+Author : Dhruv B Kakadiya
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int neg=0;
+        int pos=0;
+        int zero=0;
+        int a;
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            a = sc.nextInt();
+            if(a>0)
+                pos++;
+            else if(a<0)
+                neg++;
+            else
+                zero++;
+        }
+        float fp = (float)pos/n;
+        float fn = (float)neg/n;
+        float fz = (float)zero/n;
+        System.out.printf("%.3f\n",fp);
+        System.out.printf("%.3f\n",fn);
+        System.out.printf("%.3f\n",fz);
+    }
+}
