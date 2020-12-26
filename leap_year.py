4d986703b747232292eaa870def552a09b100e5f
DZ521111 <dhruvkakadiya3@gmail.com>
2020-12-26 09:11:05 +0530

    leap year code added

---
+++ leap_year.py
@@ -0,0 +1,21 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if (year % 4) == 0: 
+        if (year % 100) == 0: 
+            if (year % 400) == 0: 
+                return True
+            else: 
+                return False
+        else: 
+             return True
+    else: 
+        return False
+    
+    return leap
