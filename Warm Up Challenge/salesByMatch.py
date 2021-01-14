795df47f1a783c2182029ab45d27aac8d06de110
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-14 16:31:54 +0530

    sales by match code updated!

--- Warm Up Challenge/salesByMatch.py
+++ Warm Up Challenge/salesByMatch.py
@@ -2,3 +2,32 @@
 Author : Dhruv B Kakadiya
 
 '''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+from collections import Counter
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    socks = Counter(ar)
+    totalPairs = 0
+    for key, val in socks.items():
+        totalPairs += (val // 2)
+    return totalPairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
