fca7475719ddbd038b9423fcd2b86c33b099b2a2
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-16 19:31:17 +0530

    divisible sum pairs code updated!

---
+++ problem Solving/divisible_sum_pairs.py
@@ -0,0 +1,37 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+    totalPairs = 0
+    for i in range(n - 1):
+        for j in range(i, n):
+            if ((i < j) and (ar[i] + ar[j]) % k == 0):
+                totalPairs += 1
+    return (totalPairs)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
