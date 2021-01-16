d2b81a4074e9b437d2b3683e3c5a91f789b4df15
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-16 19:32:26 +0530

    bill appetit code updated!

---
+++ problem Solving/bill_part.py
@@ -0,0 +1,33 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    bill.remove(bill[k])
+    partition = sum(bill) // 2
+    if (b == partition):
+        print("Bon Appetit")
+    else:
+        print(b - partition)
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
