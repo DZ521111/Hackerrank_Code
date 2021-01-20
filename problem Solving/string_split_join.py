75e749bf1cae795a232c69699ee6878343ad3bd1
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-20 13:15:15 +0530

    string split and join code added!

---
+++ problem Solving/string_split_join.py
@@ -0,0 +1,13 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+
+def split_and_join(line):
+    # write your code here
+    return ("-".join(line.split()))
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
